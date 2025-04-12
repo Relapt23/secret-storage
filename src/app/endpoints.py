79e6a3d820de5ecf720cc899f36b56299e1b8c9b
Laptev Roman <romashlapt@mail.ru>
2025-04-12 17:57:58 +0700

    review refactoring

--- src/app/endpoints.py
+++ src/app/endpoints.py
@@ -14,10 +14,12 @@ async def create_secret(
     redis_adapter: RedisAdapter = Depends(make_redis_adapter),
 ) -> SecretKeyInfo:
     encoded_secret = base64.b64encode(secret.secret.encode("utf-8")).decode("utf-8")
+
     secret_key = await db_adapter.create(
         encoded_secret, secret.passphrase, secret.ttl_seconds
     )
     await redis_adapter.update(secret_key, encoded_secret, secret.ttl_seconds)
+
     return SecretKeyInfo(secret_key=secret_key)
 
 
@@ -28,14 +30,24 @@ async def get_secret(
     redis_adapter: RedisAdapter = Depends(make_redis_adapter),
 ) -> SecretInfo:
     secret_in_cache = await redis_adapter.get(secret_key)
+
     if secret_in_cache:
+        decoded_secret = base64.b64decode(
+            secret_in_cache.secret.encode("utf-8")
+        ).decode("utf-8")
+
         await redis_adapter.delete(secret_key)
         await db_adapter.delete(secret_key)
-        return SecretInfo(secret=secret_in_cache.secret)
+
+        return SecretInfo(secret=decoded_secret)
 
     secret_in_db = await db_adapter.get(secret_key)
     if secret_in_db:
+        decoded_secret = base64.b64decode(secret_in_db.secret.encode("utf-8")).decode(
+            "utf-8"
+        )
         await db_adapter.delete(secret_key)
+
         return SecretInfo(secret=secret_in_db)
 
     raise HTTPException(detail="secret_not_found", status_code=404)
@@ -49,6 +61,8 @@ async def delete_secret(
 ) -> DeletedSecret:
     deleted_cache_secret = await redis_adapter.delete(secret_key)
     deleted_db_secret = await db_adapter.delete(secret_key)
-    if deleted_cache_secret and deleted_db_secret:
+
+    if deleted_cache_secret or deleted_db_secret:
         return DeletedSecret(status="secret_deleted")
+
     raise HTTPException(status_code=404, detail="secret_not_deleted")
